93ef8c35828b1945b5240cab47a74d0941fbf05f
Prashant Chaudhary <pc@Prashants-MacBook-Air.local>
2017-04-27 09:28:01 +0530

    breadcrums, moved inline css to file

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,12 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
-
-$route['changePassword'] = 'welcome/changePassword';
-$route['aboutUs'] = 'welcome/aboutUs';
-
-$route['userProfile'] = 'welcome/userProfile';
-
+$route['changePassword'] = 'home/changePassword';
+$route['aboutUs'] = 'home/aboutUs';
+$route['userProfile'] = 'home/userProfile';
 $route['translate_uri_dashes'] = FALSE;
--- application/views/changePassword.php
+++ application/views/changePassword.php
@@ -6,46 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>CampusPuppy</title>
-	<link href="<?php echo base_url('/assets/css/home.css'); ?>" rel="stylesheet">
-
-	<style type="text/css">
-	input[type=password], select {
-	    width: 100%;
-	    padding: 10px;
-	    margin: 8px 0;
-	    display: inline-block;
-	    border: 1px solid #ccc;
-	    border-radius: 4px;
-	    box-sizing: border-box;
-	}
-
-	input[type=submit] {
-	    width: 100%;
-	    background-color: #283e4a;
-	    color: white;
-	    padding: 14px 20px;
-	    margin: 8px 0;
-	    border: none;
-	    border-radius: 4px;
-	    cursor: pointer;
-	}
-
-	input[type=submit]:hover {
-	    background-color: #283e4a;
-	}
-
-	input,
-	input::-webkit-input-placeholder {
-    font-size: 14px;
-	}
-
-	label{
-		font-size: 15px;
-		font-weight: bold;
-	}
-
-	</style>
-
+	<link href="<?php echo base_url('/assets/css/change-password.css'); ?>" rel="stylesheet">
 </head>
 
 <body>
@@ -54,29 +15,28 @@
 		<main class="flex main-container globalContainer">
 			<aside class="flex__item left-pane">
 				<?php echo $activeUserLeft; ?>
-				<div class="post card">
+				<div class="card">
 					<img src="/assets/img/showcase/CP1.png" alt="" style="width: 100%;">
 				</div>
 			</aside>
 			<div class="main-body flex__item">
-				<div class="post card">
-					<p style="font-size: 15px;">Home / Change Password</p>
+				<div class="card">
+					<ol class="breadcrumb">
+						<li><a href="/">Home</a></li>
+						<li class="active">Change Password</li>
+					</ol>
 				</div>
-
-				<div class="post card">
-					<center><b style="font-size: 15px;">CHANGE PASSWORD</b></center>
-
-					<form style="margin-top: 15px;">
-			    <label for="currentPassword">Current Password</label>
-			    <input type="password" id="currentPassword" placeholder="Current Password">
-					<label for="newPassword">New Password</label>
-			    <input type="password" id="newPassword" placeholder="New Password">
-					<label for="confirmNewPassword">Confirm New Password</label>
-			    <input type="password" id="confirmNewPassword" placeholder="Confirm New Password">
-
-			    <input type="submit" value="Change Password">
-			  </form>
-
+				<div class="change-password__section card">
+					<h1 class="change-password__section-title">Change password</h1>
+					<form class="change-password__form form">
+					<label for="currentPassword" class="form__label">Current Password</label>
+					<input type="password" id="currentPassword" placeholder="Current Password" class="form__input">
+					<label for="newPassword" class="form__label">New Password</label>
+					<input type="password" id="newPassword" placeholder="New Password" class="form__input">
+					<label for="confirmNewPassword" class="form__label">Confirm New Password</label>
+					<input type="password" id="confirmNewPassword" placeholder="Confirm New Password" class="form__input">
+					<input type="submit" value="Change Password" class="btn btn--primary change-password__form-submit">
+				</form>
 				</div>
 			</div>
 			<aside class="flex__item right-pane">
