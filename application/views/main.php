37656c648453de7a47480df9b7b35c044c3d2196
Prashant Chaudhary <prashantp099@gmail.com>
2017-06-18 01:31:03 +0530

    Landing page forgot psswd modal, and few ui fixes

--- application/views/main.php
+++ application/views/main.php
@@ -4,6 +4,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>CampusPuppy</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i" rel="stylesheet">
+	<link href="<?php echo base_url('/assets/css/remodal.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('/assets/css/remodal-default-theme.css'); ?>" rel="stylesheet">
 	<link href="/assets/css/landing-page.css" rel="stylesheet">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
@@ -21,8 +24,8 @@
 				<a href="<?php echo base_url(); ?>" class="flex__item">
 					<img alt="logo" src="<?php echo base_url('/assets/img/logo-white.png'); ?>" class="logo">
 				</a>
-				<div class="login-from-container flex">
-					<form method="post" action="<?php echo base_url('web/doLogin'); ?>" class="flex login-from form">
+				<div class="login-form-container flex">
+					<form method="post" action="<?php echo base_url('web/doLogin'); ?>" class="flex login-form form">
 						<div class="form-group">
 							<label class="form__label" for="email">Email</label>
 							<input type="email" id="email" required name="email" class="form__input form__input--tiny" placeholder="E-Mail Address">
@@ -30,7 +33,7 @@
 						<div class="form-group">
 							<label class="form__label" for="password">Password</label>
 							<input type="password" id="password" required class="form__input form__input--tiny" name="password" placeholder="Password">
-							<a href="javascript:" class="forgot-psswd">Forgot password</a>
+							<a href="javascript:" class="forgot-psswd js-forgot-password">Forgot password</a>
 						</div>
 						<div class="from-group flex">
 							<button type="submit" class="btn btn--primary">LOGIN</button>
@@ -45,16 +48,20 @@
 					<h2>Register</h2>
 					<form method="post" class="form register-form" action="<?php echo base_url('web/register'); ?>">
 						<div class="form-group">
-							<input type="text" class="form__input" name="name" placeholder="Full Name" required>
+							<label for="name" class="form__label">Name</label>
+							<input type="text" id="name" class="form__input" name="name" placeholder="Full Name" required>
 						</div>
 						<div class="form-group">
-							<input type="email" class="form__input" name="email" placeholder="E-Mail Address" required>
+							<label for="register-email" class="form__label">Email</label>
+							<input type="email" class="form__input" id="register-email" name="email" placeholder="E-Mail Address" required>
 						</div>
 						<div class="form-group">
-							<input type="text" class="form__input" name="mobile" placeholder="Mobile Number" maxlength="10" required>
+							<label for="mobile-number" class="form__label">Mobile Number</label>
+							<input type="text" class="form__input" id="mobile-number" name="mobile" placeholder="Mobile Number" maxlength="10" required>
 						</div>
 						<div class="form-group">
-							<input type="password" class="form__input" name="password" placeholder="Password" required>
+							<label for="register-password" class="form__label">Password</label>
+							<input type="password" id="register-password" class="form__input" name="password" placeholder="Password" required>
 						</div>
 						<div class="form-group">
 							<label for="accountType">Are you an Employer ?</label>
@@ -72,7 +79,34 @@
 		</main>
 		<?php echo $footer; ?>
 	</div>
+	<div class="remodal forgot-psswd-modal" data-remodal-id="modal">
+		<button data-remodal-action="close" class="remodal-close"></button>
+		<div class="modal-body">
+			<div class="forgot-psswd-form-container">
+				<h2>Forgot your password ?</h2>
+				<form method="post" class="form forgot-psswd-form" action="">
+					<div class="form-group">
+						<label for="forgot-psswd-email" class="form__label">Email Address</label>
+						<input type="email" class="form__input" id="forgot-psswd-email" name="email" placeholder="E-Mail Address" required>
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Send Me Instructions" class="btn btn--primary">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 	<script src="<?php echo base_url('/assets/js/jquery-3.2.0.min.js'); ?>"></script>
+	<script src="<?php echo base_url('/assets/js/remodal.min.js'); ?>"></script>
 	<script src="<?php echo base_url('/assets/js/common.js'); ?>"></script>
+	<script>
+		$(document).ready(function () {
+			$(document).on('click', '.js-forgot-password', openForgotPsswdModal);
+			function openForgotPsswdModal(ev) {
+				var modal = $('[data-remodal-id="modal"]').remodal();
+				modal.open();
+			}
+		});
+	</script>
 </body>
 </html>
