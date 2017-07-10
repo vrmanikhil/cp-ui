0b0286644125418a0d6b6f66924564c7cd1fe91e
Nikhil Verma <vrmanikhil@gmail.com>
2017-07-10 13:11:59 +0530

    footer

--- application/views/commonCode/footer.php
+++ application/views/commonCode/footer.php
@@ -1,9 +1,11 @@
 <footer class="flex__item primary-footer">
   <div class="globalContainer flex align-center">
+    <a href="#" class="footer__social-link" style="max-width: 175px !important;  color: white; font-size: 10px; display: block;"><b>This is a Beta Version</b></a>
     <div class="footer__social-links flex__item">
       <a href="https://www.facebook.com/campuspuppy" target="_blank" class="footer__social-link"><img class="footer__social-link-img" src="/assets/img/fb.png" alt=""></a>
       <a href="https://www.twitter.com/campuspuppy" target="_blank" class="footer__social-link"><img class="footer__social-link-img" src="/assets/img/twitter.png" alt=""></a>
     </div>
+
     <nav class="footer-nav">
       <a href="<?php echo base_url('about-us'); ?>" class="footer-nav__link footer-text"><b>About Us</b></a>
       <a href="<?php echo base_url('privacy-policy'); ?>" class="footer-nav__link footer-text"><b>Privacy Policy</b></a>
